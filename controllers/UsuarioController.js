dba330686c0c3ea8d01c4e0a1325e77e09e5789f
Jaime Farfan <jfarfan@utec.edu.pe>
2020-07-01 14:03:57 -0500

    modificaciones

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -19,4 +19,65 @@ usuarioController.create = function(req, res) {
     res.render('../views/usuario/create');
 };
 
+usuarioController.save = function(req, res) {
+    var usuario = new Usuario( req.body );
+
+    usuario.save(function(err) {
+        if (err) {
+            console.log('Error', err);
+            return;
+        }
+        console.log("Usuario creado satisfactoriamente");
+        res.redirect("/usuarios/show/" + usuario._id);
+        // res.redirect("/usuarios");        
+
+    });
+};
+
+usuarioController.show = function(req, res) {
+
+    Usuario.findOne({ _id: req.params.id }).exec(function(err, usuario) {
+        if (err) {
+            console.log('Error', err);
+            return;
+        }
+        res.render('../views/usuario/show',{usuario: usuario });
+
+    });
+};
+
+usuarioController.edit = function(req, res) {
+
+    Usuario.findOne({ _id: req.params.id }).exec(function(err, usuario) {
+        if (err) {
+            console.log('Error', err);
+            return;
+        }
+        res.render('../views/usuario/edit',{usuario: usuario });
+
+    });
+};
+
+usuarioController.update = function(req, res){
+
+    Usuario.findByIdAndUpdate( req.params.id, {$set: {
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        email: req.body.email,
+        estado: req.body.estado
+    }}, { new: true },
+    
+    function( err, usuario){
+        if( err ){ 
+            console.log('Error: ', err); 
+            res.render('../views/usuario/edit', {usuario: req.body} );
+        }
+        
+        console.log( usuario );
+        
+        res.redirect('/usuarios/show/' + usuario._id);
+        
+    });
+};
+
 module.exports = usuarioController;
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,5 +5,9 @@ var usuario = require('../controllers/UsuarioController');
 
 router.get('/', usuario.list);
 router.get('/create', usuario.create);
+router.post('/save', usuario.save);
+router.get('/show/:id', usuario.show);
+router.get('/edit/:id', usuario.edit);
+router.post('/update/:id', usuario.update);
 
 module.exports = router;
